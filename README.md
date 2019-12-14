8a9a8a94637b22bdd94eb244b2db6f4455ddcfa6
kreno911 <mak187@netzero.com>
2019-12-13 23:29:30 -0500

    Initial check in of new python note files

---
+++ README.md
@@ -0,0 +1,9 @@
+Python notes to cover whatever is needed to become python expert
+
+Organization
+1. Basics
+2. Advanced
+3. Libraries
+4. Networking/URLs/Server stuff
+5. Example tools - also see python-tools
+
---
+++ py_notes1.py
@@ -0,0 +1,43 @@
+
+####################################
+#   This first part is to cover all the basics needed to 
+#   understand python.
+#
+#   The only imports tested here are in the import statement below.
+#
+####################################
+
+import os, sys, glob, re, shutil, random
+
+def testOsFunctions():
+    pass
+def testSysFunctions():
+    pass
+def testGlobFunctions():
+    pass
+def testShutilFunctions():
+    pass
+def testRandomFunctions():
+    pass
+def testEnv():
+    pass
+def testArgs():
+    pass
+
+'''
+Block out...
+'''
+testOsFunctions()
+testSysFunctions()
+testGlobFunctions()
+testShutilFunctions()
+testRandomFunctions()
+testEnv()
+testArgs()
+
+# The real function to be had 
+testStringFunctions()
+testArrays()
+testSets()
+testDictionaries()
+
