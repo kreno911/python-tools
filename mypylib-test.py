a2c01c710256cdfcf9d15833069c12d3b7c906a4
kreno911 <mak2100@gmail.com>
2020-01-10 21:32:45 -0500

    Added tests

--- mypylib-test.py
+++ mypylib-test.py
@@ -1,13 +1,19 @@
 
 # Tester for mypylib functions
 # When all functions return True, we are good to go
+#
+# All tests should be named test_<actual_function_name>_<test_case>
+#
+# Run as: python3 -m unittest mypylib-test.py
 
 import unittest
 from mypylib import *
 
 class TestMypylib(unittest.TestCase):
+    # Basic test between two files. May need to create/modify
+    # to get test to work correctly. 
     def test_files_older_than_a(self):
-        print("Test 1")
+        print("Test files_older_than_a")
         #file1 = "test_data/temp_data.csv"
         file1 = "test_data/test1"
         file2 = "test_data/test2"
@@ -15,11 +21,23 @@ class TestMypylib(unittest.TestCase):
             print("%s is older than %s" % (file1, file2))
         else:
             print("%s is not older than %s" % (file1, file2))
+    # Test FileNotFoundError exception thrown for non-existent file
     def test_files_older_than_b(self):
-        print("Test 2")
-        # Test exception thrown for non-existent file
+        print("Test files_older_than_b")
         file1 = "test_data/test"
         file2 = "test_data/test2"
         with self.assertRaises(FileNotFoundError):
             is_older(file1, file2)
-    
+    # Test minutes old for file - create file an hour or so old
+    # to get successful values.
+    def test_get_age_of_file_a(self):
+        print("Test get_age_of_file_a")
+        afile = "test_data/test1"
+        print("Testing get_age_of_file() minutes...")
+        print("%s is %d minutes old" % (afile, get_age_of_file(afile)))
+        print("      %d seconds old" % get_age_of_file(afile, unit='s'))
+        print("      %d hours old" % get_age_of_file(afile, unit='H'))
+        print("      %d days old" % get_age_of_file(afile, unit='d'))
+    # Test that ValueError is raised if unit is not correct
+    def test_get_age_of_file_b(afile):
+        pass
--- mypylib.py
+++ mypylib.py
@@ -1,9 +1,16 @@
 
 from datetime import datetime
-import os, re
+import os, re, stat, time
+
+# Important links
+#   https://www.techbeamers.com/python-time-functions-usage-examples/#time-time-function
 
 ####
 # Check if first file is older than second.
+# Param 1: First file for comparison 
+# Param 2: Second file for comparison
+# Returns: True if first file is older than second 
+# Raises: FileNotFoundError if either file does not exist
 ####
 def is_older(file1, file2):
     """ Return True if first file is older than second file.
@@ -17,11 +24,49 @@ def is_older(file1, file2):
         print("Exception: Check your files: [%s,%s]" % (file1,file2))
         raise FileNotFoundError
 ####
-# Return all file paths that are older the this_file.
+# Return file age based on unit (default to minutes)
+# Param 1: file to check age of
+# Param 2: unit to check for: mM - minutes, default
+#                             sS - seconds
+#                             hH - hours
+#                             dD - days
+# Raises: ValueError if unit not found
+#
+# time.time() returns # seconds since 1970 as floating point.
+#   Ex: 1504634843.1003768
+# Note: 
+#   Age is NOT exact and time is truncated. Should be fine for most uses.
 ####
-def files_older_than(this_file, list_of_files):
+def get_age_of_file(file, unit="M"):
+    """ Return unit age of given file. 
+        unit defaults to 'm|M' - minutes
+        's|S' - seconds
+        'h|H' - hours
+        'd|D' - days
+        This function truncates mantissa on all times int(x) and is not exact. 
+        Values of 1.9 will return 1. Use this function when inexact times are sufficient. 
+        """
+    if unit not in ("m","M","s","S","h","H","d","D"):
+        raise ValueError("unit not correct! [m|M|s|S|h|H|d|D]")
+    # Seconds old
+    if unit == "S" or unit == "s":
+        return int((time.time() - os.stat(file)[stat.ST_MTIME]))
+    # Hours old
+    if unit == "H" or unit == "h":
+        return int((time.time() - os.stat(file)[stat.ST_MTIME])/60/60)
+    # Days old
+    if unit == "D" or unit == "d":
+        return int((time.time() - os.stat(file)[stat.ST_MTIME])/60/60/24)
+    # Minutes
+    return int((time.time() - os.stat(file)[stat.ST_MTIME])/60)
+####
+# Return all file that are older given date.
+# Param 1: Date in format
+# Param 2: List of files with absolute paths
+# Returns: List of strings of file paths
+####
+def files_older_than(older_than_date, list_of_files):
     for filename in list_of_files:
         if os.path.getmtime(os.path.join(os.path, filename)) < now - 7 * 86400:
             if os.path.isfile(os.path.join(os.path, filename)):
                 print("%s is older than %s" % (this_file,filename))
-
