3a8c7517f076ee6b14b415f107d7720f3732077a
kreno911 <mak2100@gmail.com>
2020-02-01 12:05:33 -0500

    Added some imports and comments

--- python_import_test.py
+++ python_import_test.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 
 import sys, os, datetime, datetime, re, json
-import random, time, getopt, fcntl, struct
+import random, time, getopt, struct
 import glob
 from socket import *
 import numpy
 
+# This lib does not work on windows
+#import fcntl
+
+import sqlalchemy 
+
 # from NetCDF4 import Dataset
 
+# Not on windows 
 import smtplib
 from email.MIMEMultipart import MIMEMultipart
 import urllib2, cookielib, platform
