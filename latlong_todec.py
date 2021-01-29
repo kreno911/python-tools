906f704cd16527849241701a8b21049a4ea18876
Mike CTR Krauss <mike.ctr.krauss@faa.gov>
2021-01-29 11:42:49 -0500

    Python 3 updates

--- latlong_todec.py
+++ latlong_todec.py
@@ -8,12 +8,14 @@ import sys
 #   51-52-16.648N, 176-40-26.743W
 def convertLatToDec(lat):
     direction = {'N':1, 'S':-1, 'E': 1, 'W':-1}
-    lat_parts = lat.split('-')
+    # Remove any ','
+    lat_parts = lat.replace(",", "").split('-')
     # Pick off last char which is the direction
-    dir = lat_parts[2][-1]
+    currdir = lat_parts[2][-1]
     lat_parts[2] = lat_parts[2][:-1]  # This actually picks off last char
+    #print("%s, [%s]" % (currdir, lat_parts))
 
-    return "%s" % str((int(lat_parts[0])+int(lat_parts[1])/60.0+int(float(lat_parts[2]))/3600.0) * direction[dir] )
+    return "%s" % str((int(lat_parts[0])+int(lat_parts[1])/60.0+int(float(lat_parts[2]))/3600.0) * direction[currdir])
 
 def convertLongToDec(long):
     direction = {'N':1, 'S':-1, 'E': 1, 'W':-1}
@@ -25,12 +27,12 @@ def convertLongToDec(long):
 
 # cmd counts as an arg
 if (len(sys.argv) != 3):
-    print "todec <lat>,<long> [lat|long value in deg/min/sec (51-52-16.648N)]"
-    print "    example: ./latlong_todec.py 51-52-16.648N 176-40-26.743W"
+    print ("todec <lat>,<long> [lat|long value in deg/min/sec (51-52-16.648N)]")
+    print ("    example: ./latlong_todec.py 51-52-16.648N 176-40-26.743W")
     sys.exit(11)
 
 arg1 = sys.argv[1]
 arg2 = sys.argv[2]
-#print "%s, %s" % (arg1,arg2)
-print "(%s,%s)" % (convertLatToDec(arg1), convertLongToDec(arg2))
+#print ("%s, %s" % (arg1,arg2))
+print ("(%s,%s)" % (convertLatToDec(arg1), convertLongToDec(arg2)))
 
